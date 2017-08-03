f24e5e923bc735ef8a1879218711e676e3f9f15c
Nicholas Holloway <nholloway91@gmail.com>
2017-08-03 14:59:44 +0100

    Paired TDD functions activity

---
+++ my_functions.rb
@@ -0,0 +1,53 @@
+
+def add_array_lengths (array_1, array_2)
+
+  result = array_1.length + array_2.length
+
+end
+
+def add_array_lengths (array_1, array_2)
+
+  result = array_1.length + array_2.length
+
+end
+
+
+def sum_array(numbers)
+  sum = 0
+  for number in numbers do
+    sum += number
+  end
+  return sum
+end
+
+
+def sum_array(numbers)
+  sum = 0
+  for number in numbers do
+    sum += number
+  end
+  return sum
+end
+
+
+def find_item(array, item)
+  for houses in array do
+    if houses == item
+      return true
+    end
+  end
+  return false
+end
+
+def find_item(array, item)
+  for houses in array do
+    if houses == item
+      return true
+    end
+  end
+  return false
+end
+
+def get_first_key(wallets)
+  return wallets.keys[0]
+end
